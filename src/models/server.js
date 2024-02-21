e718fed1a85934169a18014d66cf6439b8e1e4e8
Bryan Hurtado Alvarado <brayan.ha.1996@gmail.com>
2024-02-21 17:54:28 -0500

    first

---
+++ src/models/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import dbConnection from '../db/config';
+class Server{
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT
+        this.paths = {
+            auth:       '/api/auth',
+            categorys:  '/api/categorys',
+            products:   '/api/products',
+            users:      '/api/users'
+        }
+        //Conectar db
+        this.conectarDb()
+        //MIDDLEWARES
+        this.middlewares()
+        this.routes()
+    }
+    async conectarDb(){
+        await dbConnection()
+    }
+
+    middlewares(){
+        //CORS
+        this.app.use(cors())
+        //LECTURA Y PARSEO DEL BODY
+        this.app.use( express.json() )
+        //DIRECTORIO PUBICO
+        // this.app.use( express.static('public') )
+    }
+    routes(){
+        // this.app.use(this.paths.auth, require('../routes/auth.routes'))
+        // this.app.use(this.paths.categorys, require('../routes/categorys.routes'))
+        // this.app.use(this.paths.users, require('../routes/users.routes'))
+        // this.app.use(this.paths.products, require('../routes/productos.routes'))
+
+    }
+    listen(){
+        this.app.listen( this.port, ()=>{
+            console.log('SERVIDOR', this.port)
+        })
+    }
+}
+export default Server;
